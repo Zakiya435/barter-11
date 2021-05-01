da4179b9d5f412cd9d72c8a87c61c3e35726f49f
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2021-05-01 11:38:23 +0530

    Update CustomSideBarMenu.js

--- components/CustomSideBarMenu.js
+++ components/CustomSideBarMenu.js
@@ -1,13 +1,84 @@
 import React, { Component} from 'react';
 import {StyleSheet, View, Text,TouchableOpacity} from 'react-native';
 import { DrawerItems} from 'react-navigation-drawer'
-
+import { Avatar } from 'react-native-elements';
+import * as ImagePicker from 'expo-image-picker';
+import db from '../config';
 import firebase from 'firebase';
 
 export default class CustomSideBarMenu extends Component{
+  state={userId:firebase.auth().currentUser.email,
+    image:null,
+    name:"",
+    docId:""
+    }
+    
+  selectPicture=async()=>{
+      const{cancelled,uri} = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!cancelled) {
+        this.setState({
+          image:uri
+        })
+        this.uploadImage(uri,this.state.userId)
+      }
+    }
+  
+  uploadImage=async(uri,imageName)=>{
+      var response=await fetch(uri)
+      var blob = await response.blob()
+      var ref = firebase.storage().ref().child("user_profiles"+ imageName)
+       return(ref.put(blob).then((response)=>{
+         this.fetchImage(imageName)
+       }))
+    }
+
+    fetchImage=(imageName)=>{
+      var storageRef = firebase.storage().ref().child("user_profiles"+imageName)
+      storageRef.getDownloadURL().then((uri)=>{
+        this.setState({
+          image:uri
+        })
+        .catch((error)=>{
+          this.setState({image:"#"})
+        })
+  
+      })
+    }
+  
+    getUserProfile(){
+      db.collection('user').where('emailId','==',this.state.userId).onSnapshot((querieSnapshot)=>{
+        querieSnapshot.forEach((doc)=>{
+          this.setState({
+            name:doc.data().firstName+" "+doc.data().lastName
+          })
+        })
+      })
+    }
+    componentDidMount(){
+      this.fetchImage(this.state.userId)
+      this.getUserProfile()
+    }
+
+  
   render(){
     return(
       <View style={{flex:1}}>
+        <View style={{flex:0.5,alignItems:'center',backgroundColor:'orange'}}>
+        <Avatar 
+        rounded
+        source={{uri:this.state.image}}
+        size="medium"
+        onPress={()=>this.selectPicture()}
+        containerStyle={styles.imageContainer}
+        showEditButton
+        />
+        <Text style={{fontWeight:100,fontSize:20,paddingTop:10}}>{this.state.name}</Text>
+        </View>
         <View style={styles.drawerItemsContainer}>
           <DrawerItems {...this.props}/>
         </View>
